1468c91c7e543a9a3c7dc732e691b9b6e8c1c865
Ondřej Mottl <ondrej.mottl@gmail.com>
2025-12-14 15:15:13 +0100

    Global RoC: add script structure, add steps to fit model, start the visualization section

--- R/Global_roc/Plot_global_roc.R
+++ R/Global_roc/Plot_global_roc.R
@@ -1,8 +1,37 @@
+#----------------------------------------------------------#
+#
+#
+#               TIBS - Presentation- 20260107
+#
+#             Make a visualisation of Mottl et al 2023
+#
+#
+#                       O. Mottl
+#                         2025
+#----------------------------------------------------------#
+
+# Make a step-by-step visualisation of the global RoC data from
+# "Global acceleration in rates of vegetation change over the past 18,000 years"
+
+
+#----------------------------------------------------------#
+# 1. Set up -----
+#----------------------------------------------------------#
+
+library(here)
+
+source(
+  here::here("R/___setup_project___.R")
+)
+
 age_treshold <- 18e3
 roc_treshold <- 2
-
 time_bin <- 500
 
+
+#----------------------------------------------------------#
+# 2. Get data -----
+#----------------------------------------------------------#
 
 # Data from "Global acceleration in rates of vegetation change over the past 18,000 years"
 # version 1.1.1
@@ -15,16 +44,12 @@ data_global_roc <-
   janitor::clean_names()
 
 
-names(data_global_roc)
-
-
 data_roc_with_bin <-
   data_global_roc |>
   dplyr::select(region, dataset_id, roc_main) |>
   tidyr::unnest(cols = roc_main) |>
   janitor::clean_names() |>
   dplyr::mutate(bin = ceiling(age / time_bin) * time_bin)
-
 
 data_roc_sum <-
   data_roc_with_bin |>
@@ -37,7 +62,6 @@ data_roc_sum <-
     roc_upq = quantile(roc, 0.95, na.rm = TRUE),
     roc_sd = sd(roc, na.rm = TRUE)
   )
-
 
 data_peak_sum <-
   data_roc_with_bin |>
@@ -72,8 +96,66 @@ data_roc_to_fit <-
 # 2. RoC per continent -----
 #----------------------------------------------------------#
 
-plot_europe <-
-  draw_region(
-    sel_region = "Europe",
-    data_binned = data_roc_to_fit
+data_models <-
+  data_roc_to_fit |>
+  dplyr::group_by(region) |>
+  tidyr::nest() |>
+  dplyr::ungroup() |>
+  dplyr::mutate(
+    mod = purrr::map(
+      .x = data,
+      .f = ~ fit_gam_model(
+        var_y = "roc_upq",
+        var_x = "bin",
+        family = "tw()",
+        data = .x,
+        weights = "n_roc"
+      )
+    )
   )
+
+data_mod_predictions <-
+  data_models |>
+  dplyr::mutate(
+    data_pred = purrr::map(
+      .x = mod,
+      .f = ~ predict_gam(
+        gam_model = .x,
+        var_x = "bin",
+        deriv = TRUE
+      )
+    )
+  ) |>
+  tidyr::unnest_wider(data_pred, names_repair = "unique⁠") |>
+  rlang::set_names(
+    nm = c(
+      "region",
+      "data",
+      "mod",
+      "data_pred",
+      "k",
+      "p_value",
+      "data_first_derivative"
+    )
+  )
+#----------------------------------------------------------#
+# 3. Visualisation -----
+#----------------------------------------------------------#
+
+
+palette_regions <-
+  colorRampPalette(colours[-c(1:3)])(6) |>
+  rlang::set_names(
+    nm = c(
+      "Africa",
+      "Asia",
+      "Europe",
+      "North America",
+      "Oceania",
+      "South America"
+    )
+  )
+
+plot(seq_len(length(palette_regions)), rep_len(1, length(palette_regions)),
+  col = palette_regions, pch = 16, cex = 3, xaxt = "n", yaxt = "n", xlab = "", ylab = ""
+)
