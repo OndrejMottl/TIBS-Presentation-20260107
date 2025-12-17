22fd9cd93e55df6170493cff8d2a4c66b7840156
OndrejMottl <ondrej.mottl@gmail.com>
2025-12-17 10:48:55 +0100

    Power of personal PCs

---
+++ Presentation/Materials/Computers/README.md
@@ -0,0 +1,9 @@
+# Source of Data for Computer Power
+
+The data in `pc_power.csv` contains information about the computational power of personal computers over time. The data has been obtained from:
+
+- <https://en.wikipedia.org/wiki/Instructions_per_second>
+- <https://www.hp.com/us-en/shop/tech-takes/specifications-personal-computers-over-time>
+- <https://en.wikipedia.org/wiki/List_of_Intel_processors>
+- <https://www.spec.org/cpu/>
+- <https://en.wikipedia.org/wiki/History_of_personal_computers>
---
+++ R/Computers/PC_power_in_time.R
@@ -0,0 +1,122 @@
+#----------------------------------------------------------#
+#
+#
+#               TIBS - Presentation- 20260107
+#
+#             Visualisation PC power in time
+#
+#                       O. Mottl
+#                         2025
+#----------------------------------------------------------#
+
+# Plot a timeline of computer power change
+
+#----------------------------------------------------------#
+# 1. Set up -----
+#----------------------------------------------------------#
+
+library(here)
+
+source(
+  here::here("R/___setup_project___.R")
+)
+
+
+
+#----------------------------------------------------------#
+# 2. Get  data -----
+#----------------------------------------------------------#
+
+data_pc <-
+  readr::read_csv(
+    here::here(
+      "Presentation/Materials/Computers/pc_power.csv"
+    )
+  ) |>
+  janitor::clean_names()
+
+
+#----------------------------------------------------------#
+# 3. Make a plot -----
+#----------------------------------------------------------#
+
+library(ggview)
+
+p_0 <-
+  data_pc |>
+  dplyr::mutate(
+    mips_avg = (low_mips + high_mips) / 2
+  ) |>
+  ggplot2::ggplot(
+    mapping = ggplot2::aes(
+      x = year
+    )
+  ) +
+  theme_presentation() +
+  ggplot2::theme(
+    legend.position = "none",
+    panel.grid.minor = ggplot2::element_blank()
+  ) +
+  ggplot2::labs(
+    caption = "MIPS = Million Instructions Per Second",
+    x = "Year",
+    y = "Computing Power (MIPS)"
+  ) +
+  ggplot2::scale_color_gradient(
+    low = colours["purple"],
+    high = colours["orange"],
+    na.value = colours["white"],
+    limits = c(0, max(data_pc$high_mips))
+  ) +
+  ggplot2::scale_y_continuous(
+    transform = "log",
+    breaks = c(1, 1e1, 1e2, 1e3, 1e4, 1e5, 1e6, 1e7, 1e8),
+    labels = scales::comma_format()
+  ) +
+  ggplot2::geom_smooth(
+    mapping = ggplot2::aes(
+      y = mips_avg
+    ),
+    linewidth = 0.1,
+    color = colours["green"],
+    method = "loess",
+    se = FALSE,
+    formula = y ~ x
+  ) +
+  ggforce::geom_link(
+    mapping = ggplot2::aes(
+      xend = year,
+      y = low_mips,
+      yend = high_mips,
+      col = ggplot2::after_stat(y)
+    ),
+    n = 100,
+    alpha = 0.8,
+    linewidth = 2
+  ) +
+  ggplot2::geom_label(
+    mapping = ggplot2::aes(
+      label = cpu,
+      y = mips_avg
+    ),
+    alpha = 0.8,
+    size = 3
+  ) +
+  ggview::canvas(
+    width = 16,
+    height = 9,
+    dpi = 300,
+    units = "cm",
+    scale = 1
+  )
+
+#----------------------------------------------------------#
+# 4. Save the plot -----
+#----------------------------------------------------------#
+
+ggview::save_ggplot(
+  plot = p_0,
+  file = here::here(
+    "Presentation/Materials/R_generated/Computers/PC_power_in_time.png"
+  )
+)
